4ee7b410e4d5015f1f0fed5ddeade5a59d3b6fcc
luz enderley colorado torres <enderleycolorado@gmail.com>
2017-04-12 15:35:43 -0500

    Se creo la actividad DetallesActivity con activity_detalles como layout. Se completo la cartalera y se pulio el item de la cuadricula.

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/CineManagerActivity.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/CineManagerActivity.java
@@ -1,7 +1,10 @@
 package com.pmovil.camiluz.cinesmagic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -16,12 +19,22 @@ public class CineManagerActivity extends AppCompatActivity {
         ArrayList<Pelicula> cartelera = new ArrayList<Pelicula>();
         cartelera.add(new Pelicula("Ghost in the Shell", R.drawable.cartel_ghost, true, false));
         cartelera.add(new Pelicula("Logan", R.drawable.cartel_logan, true, false));
+        cartelera.add(new Pelicula("Power Rangers", R.drawable.cartel_power, true, true));
+        cartelera.add(new Pelicula("Life", R.drawable.cartel_life, false, true));
 
         Pelicula ghost = new Pelicula("Ghost in the Shell", R.drawable.cartel_ghost, true, false);
         PeliculaAdapter adaptadorDePeliculas = new PeliculaAdapter(this, cartelera);
 
         GridView diseñoCuadricula = (GridView) findViewById(R.id.diseño_cuadricula);
         diseñoCuadricula.setAdapter(adaptadorDePeliculas);
+
+        diseñoCuadricula.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent = new Intent(CineManagerActivity.this, DetallesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/DetallesActivity.java
@@ -0,0 +1,13 @@
+package com.pmovil.camiluz.cinesmagic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetallesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+    }
+}
