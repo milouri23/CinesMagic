3ca6777e62d420079ba62d57e191b8dca359ec04
Camilo Uribe Diaz <milouri23@gmail.com>
2017-04-12 09:10:36 -0500

    Entrega inicial

---
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/CineManagerActivity.java
@@ -0,0 +1,13 @@
+package com.pmovil.camiluz.cinesmagic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CineManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cine_manager);
+    }
+}
