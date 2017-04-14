5012fff9d2bc9cc90be7612f1dde96516d949fab
Camilo Uribe Diaz <milouri23@gmail.com>
2017-04-14 16:51:54 -0500

    Se agregó la fecha de mañana y pasado mañana en las reservas

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
@@ -21,21 +21,31 @@ public class ReservasFragment extends Fragment {
                              Bundle savedInstanceState) {
         View ReservasLayout = inflater.inflate(R.layout.fragment_reservas, container, false);
 
-        Date fecha = new Date();
-        String[] arregloFecha = fecha.toString().split(" ");
-        String mesDiaActual = arregloFecha[1] + " " + arregloFecha[2];
+        final long miliEn1Dia = 86400000;
+        Date fechaHoy = new Date();
+        Date fechaManana = new Date(fechaHoy.getTime() + miliEn1Dia);
+        Date fechaPasadoManana = new Date(fechaManana.getTime() + miliEn1Dia);
 
-        Log.v("ReservasFragment", fecha.toString());
-        Log.v("ReservasFragment", mesDiaActual);
+        Log.v("ReservasFragment", fechaHoy.toString());
+
         Pelicula pelicula = DetallesActivity.pelicula;
         TextView tituloPelicula = (TextView) ReservasLayout.findViewById(R.id.titulo_pelicula);
         ImageView imagenReserva = (ImageView) ReservasLayout.findViewById(R.id.cartel_reservas_imagen);
         Button botonDia1 = (Button) ReservasLayout.findViewById(R.id.boton_dia_1);
+        Button botonDia2 = (Button) ReservasLayout.findViewById(R.id.boton_dia_2);
+        Button botonDia3 = (Button) ReservasLayout.findViewById(R.id.boton_dia_3);
 
-        botonDia1.setText(mesDiaActual);
+        botonDia1.setText(obtenerMesDia(fechaHoy));
+        botonDia2.setText(obtenerMesDia(fechaManana));
+        botonDia3.setText(obtenerMesDia(fechaPasadoManana));
         tituloPelicula.setText(pelicula.getTitulo());
         imagenReserva.setImageResource(pelicula.getImageCartelResourceId());
         return ReservasLayout;
+    }
+
+    private String obtenerMesDia(Date fecha) {
+        String[] arregloFecha = fecha.toString().split(" ");
+        return arregloFecha[1] + " " + arregloFecha[2];
     }
 
 }
