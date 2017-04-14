d50754c48152aa8ea0e89730eb59a30627f47a73
Camilo Uribe Diaz <milouri23@gmail.com>
2017-04-14 16:23:58 -0500

    Se agregó la fecha de hoy en las reservas

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Date;
 
 public class ReservasFragment extends Fragment {
 
@@ -17,10 +21,18 @@ public class ReservasFragment extends Fragment {
                              Bundle savedInstanceState) {
         View ReservasLayout = inflater.inflate(R.layout.fragment_reservas, container, false);
 
+        Date fecha = new Date();
+        String[] arregloFecha = fecha.toString().split(" ");
+        String mesDiaActual = arregloFecha[1] + " " + arregloFecha[2];
+
+        Log.v("ReservasFragment", fecha.toString());
+        Log.v("ReservasFragment", mesDiaActual);
         Pelicula pelicula = DetallesActivity.pelicula;
         TextView tituloPelicula = (TextView) ReservasLayout.findViewById(R.id.titulo_pelicula);
         ImageView imagenReserva = (ImageView) ReservasLayout.findViewById(R.id.cartel_reservas_imagen);
+        Button botonDia1 = (Button) ReservasLayout.findViewById(R.id.boton_dia_1);
 
+        botonDia1.setText(mesDiaActual);
         tituloPelicula.setText(pelicula.getTitulo());
         imagenReserva.setImageResource(pelicula.getImageCartelResourceId());
         return ReservasLayout;
