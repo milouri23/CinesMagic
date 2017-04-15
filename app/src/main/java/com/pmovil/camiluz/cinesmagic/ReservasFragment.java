f0eb3283a5c35f7d7be23b577ec76a24600d839a
luz enderley colorado torres <enderleycolorado@gmail.com>
2017-04-15 13:53:27 -0500

    Se crean elementos RadioButton para seleccionar la hora de la reserva.

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.Date;
