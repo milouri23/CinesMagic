263bda51f055e96a75e0ce89b061cbf3acc0c24b
Camilo Uribe Diaz <milouri23@gmail.com>
2017-04-15 15:12:36 -0500

    Botones bonitos

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -20,7 +22,7 @@ public class ReservasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View ReservasLayout = inflater.inflate(R.layout.fragment_reservas, container, false);
+        View reservasLayout = inflater.inflate(R.layout.fragment_reservas, container, false);
 
         final long miliEn1Dia = 86400000;
         Date fechaHoy = new Date();
@@ -29,19 +31,42 @@ public class ReservasFragment extends Fragment {
 
         Log.v("ReservasFragment", fechaHoy.toString());
 
+        final int colorResourceId = android.R.color.background_dark;
         Pelicula pelicula = DetallesActivity.pelicula;
-        TextView tituloPelicula = (TextView) ReservasLayout.findViewById(R.id.titulo_pelicula);
-        ImageView imagenReserva = (ImageView) ReservasLayout.findViewById(R.id.cartel_reservas_imagen);
-        Button botonDia1 = (Button) ReservasLayout.findViewById(R.id.boton_dia_1);
-        Button botonDia2 = (Button) ReservasLayout.findViewById(R.id.boton_dia_2);
-        Button botonDia3 = (Button) ReservasLayout.findViewById(R.id.boton_dia_3);
+        TextView tituloPelicula = (TextView) reservasLayout.findViewById(R.id.titulo_pelicula);
+        ImageView imagenReserva = (ImageView) reservasLayout.findViewById(R.id.cartel_reservas_imagen);
 
-        botonDia1.setText(obtenerMesDia(fechaHoy));
-        botonDia2.setText(obtenerMesDia(fechaManana));
-        botonDia3.setText(obtenerMesDia(fechaPasadoManana));
+        final Button[] botonesDias = {(Button) reservasLayout.findViewById(R.id.boton_dia_1),
+                (Button) reservasLayout.findViewById(R.id.boton_dia_2),
+                (Button) reservasLayout.findViewById(R.id.boton_dia_3)};
+        botonesDias[0].setText(obtenerMesDia(fechaHoy));
+        botonesDias[1].setText(obtenerMesDia(fechaManana));
+        botonesDias[2].setText(obtenerMesDia(fechaPasadoManana));
         tituloPelicula.setText(pelicula.toString());
         imagenReserva.setImageResource(pelicula.getImageCartelResourceId());
-        return ReservasLayout;
+
+        RadioGroup grupoCasillas = (RadioGroup) reservasLayout.findViewById(R.id.grupo_de_casillas);
+        for(String horarioProyeccion:pelicula.getHorasProyeccion()) {
+            RadioButton casillaSeleccion = new RadioButton(getContext());
+            casillaSeleccion.setText(horarioProyeccion);
+            grupoCasillas.addView(casillaSeleccion);
+        }
+
+        for (Button botonDia:botonesDias) {
+            botonDia.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for (Button botonDia:botonesDias) {
+                        if ((Button) v == botonDia) {
+                            botonDia.setBackgroundColor(ContextCompat.getColor(getContext(), android.R.color.black));
+                        } else {
+                            botonDia.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+                        }
+                    }
+                }
+            });
+        }
+        return reservasLayout;
     }
 
     private String obtenerMesDia(Date fecha) {
