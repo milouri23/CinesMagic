884219dcb686f1169d5afef8528b895a3078770e
Camilo Uribe Diaz <milouri23@gmail.com>
2017-04-15 19:31:34 -0500

    Layout de reservas listo (mvp)

--- app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
+++ app/src/main/java/com/pmovil/camiluz/cinesmagic/ReservasFragment.java
@@ -14,11 +14,13 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Date;
 
 public class ReservasFragment extends Fragment {
 
+    private int cantidadReservas = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,15 +32,44 @@ public class ReservasFragment extends Fragment {
         Date fechaPasadoManana = new Date(fechaManana.getTime() + miliEn1Dia);
 
         Log.v("ReservasFragment", fechaHoy.toString());
+        final int MIN_RESERVAS = 1;
+        final int MAX_RESERVAS = 5;
 
-        final int colorResourceId = android.R.color.background_dark;
         Pelicula pelicula = DetallesActivity.pelicula;
-        TextView tituloPelicula = (TextView) reservasLayout.findViewById(R.id.titulo_pelicula);
         ImageView imagenReserva = (ImageView) reservasLayout.findViewById(R.id.cartel_reservas_imagen);
-
+        TextView tituloPelicula = (TextView) reservasLayout.findViewById(R.id.titulo_pelicula);
+        final TextView fechaSeleccionada = (TextView) reservasLayout.findViewById(R.id.fecha_seleccionada);
         final Button[] botonesDias = {(Button) reservasLayout.findViewById(R.id.boton_dia_1),
                 (Button) reservasLayout.findViewById(R.id.boton_dia_2),
                 (Button) reservasLayout.findViewById(R.id.boton_dia_3)};
+        Button botonDecremento = (Button) reservasLayout.findViewById(R.id.boton_decremento);
+        Button botonIncremento = (Button) reservasLayout.findViewById(R.id.boton_incremento);
+        final TextView cantidadTextView = (TextView) reservasLayout.findViewById(R.id.text_view_cantidad);
+
+        botonDecremento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantidadReservas == MIN_RESERVAS) {
+                    String warning = "No puedes reservar menos de " + MIN_RESERVAS + " boletas";
+                    Toast.makeText(getContext(), warning, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                cantidadTextView.setText(Integer.toString(--cantidadReservas));
+            }
+        });
+
+        botonIncremento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantidadReservas == MAX_RESERVAS) {
+                    String warning = "No puedes reservar más de " + MAX_RESERVAS + " boletas";
+                    Toast.makeText(getContext(), warning, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                cantidadTextView.setText(Integer.toString(++cantidadReservas));
+            }
+        });
+
         botonesDias[0].setText(obtenerMesDia(fechaHoy));
         botonesDias[1].setText(obtenerMesDia(fechaManana));
         botonesDias[2].setText(obtenerMesDia(fechaPasadoManana));
@@ -50,6 +81,9 @@ public class ReservasFragment extends Fragment {
             RadioButton casillaSeleccion = new RadioButton(getContext());
             casillaSeleccion.setText(horarioProyeccion);
             grupoCasillas.addView(casillaSeleccion);
+            casillaSeleccion.setTextSize(20);
+            casillaSeleccion.setPadding(24, 0, 0, 0);
+            casillaSeleccion.performClick();
         }
 
         for (Button botonDia:botonesDias) {
@@ -63,15 +97,17 @@ public class ReservasFragment extends Fragment {
                             botonDia.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
                         }
                     }
+                    fechaSeleccionada.setText(( (Button) v).getText());
                 }
             });
         }
+
+        botonesDias[0].performClick();
+
         return reservasLayout;
     }
-
     private String obtenerMesDia(Date fecha) {
         String[] arregloFecha = fecha.toString().split(" ");
         return arregloFecha[1] + " " + arregloFecha[2];
     }
-
 }
